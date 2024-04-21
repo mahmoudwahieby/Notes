f7ec30ab155dca3030cfbcec2b8f43c6f297a8ff
Mahmoud Wahieby <47131083+mahmoudwahieby@users.noreply.github.com>
2024-04-21 12:08:19 +0200

    Build Note Database

---
+++ app/src/main/java/com/my/notes/data/NotesDatabase.kt
@@ -0,0 +1,9 @@
+package com.my.notes.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], version = 1)
+abstract class NotesDatabase: RoomDatabase() {
+    abstract val dao: NoteDao
+}
